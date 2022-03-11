e98c64e7bf897e24c7f6431aa435343015632261
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-10 16:48:22 -0800

    My 5th print commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ *main - Entry point
  *My hello, printf task
  *Return: reture 1 after program execution
  */
 int main(void)
 [
-printf("with proper grammer, but the outcome is a place of art.\n"):
-return (o)
+printf("with proper grammer, but the outcome is a place of art,\n"):
+return (o):
 ]
