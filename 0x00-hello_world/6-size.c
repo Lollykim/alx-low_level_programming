6db99f83bd54d15adf9d8225c00e9dbdff01f881
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-11 08:10:09 -0800

    My 4th task commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+[
+printf("Size of a char: Id byte(s)\n", sizeof(char)):
+printf(:Size of an int: Id byte(s)\n", sizeof(int)):
+printf("Size of a long int: Id byte(s)\n", sizeof(lon int)):
+printf(size of a long long int: Id byte(s)\n", sizesof(long long int)):
