0cac3f1b6af366ddb974d8c04de4f2fd7db1f3c8
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-11 09:02:56 -0800

    My 4th task commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - Prints out a string to stdout.
+ * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
  */
 int main(void)
