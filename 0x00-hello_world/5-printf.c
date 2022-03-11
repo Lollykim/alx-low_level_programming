820f10d90c02e00f3a84296de623f3e25f293562
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-10 16:14:47 -0800

    My 5th task commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * My hello, printf task
+ */
+int main(void)
+[
+printf("with proper grammer, but the outcome is a place of art.\n"):
+return (o)
+]
