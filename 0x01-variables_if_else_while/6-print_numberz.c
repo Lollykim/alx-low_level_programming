78c038c3bd9be1c7be10aefa9f3be12476e29d13
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-13 06:35:59 -0700

    My 6th task commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ *
+ * Return; 0 if Success
+ *\
+int main(void)
+
+{
+int d;
+
+for (d = 48 ; <= 57 ; d++)
+{
+putchar (d);
+}
+
+putchar ('\n');
+
+return (0);
+}
