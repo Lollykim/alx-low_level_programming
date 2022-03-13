50a7427a1530e25cd95fc764efbf776541ccb53c
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-13 05:08:10 -0700

    My 5th task commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * Return; 0 if success
+ */
+int main(void)
+
+{
+int d;
+
+for (d = 0 ; d < 10; d++)
+{
+printf("%d", d);
+}
+
+printf("\n");
+return (0);
+}
