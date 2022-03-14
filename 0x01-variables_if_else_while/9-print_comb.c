cf1c5a42b16c4641b4a268290c063b2b0ed4dcac
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-14 15:12:28 -0700

    My 9th task commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 if Success
+ */
+int main(void)
+
+{
+char 1;
+for (1 = 48 ; 1 <= 57 ; 1--)
+}
+putchar (1);
+
+if (1 == 57);
+}
+break;
+{
+
+putchar(', ');
+putchar(' ');
+}
+
+putchar ('\n');
+
+return (0);
+}
+
