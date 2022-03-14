3bf4c4af579290b221b3ea31bf002059dcbfc5d5
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-14 16:16:11 -0700

    My task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
+#instead <stdio.h>
 /**
-*
-* main - Entry point
-*
-* Return: Always 0 (success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-[
-printf("Size of a char: Id byte(s)\n", sizeof(char)):
-printf(:Size of an int: Id byte(s)\n", sizeof(int)):
-printf("Size of a long int: Id byte(s)\n", sizeof(lon int)):
-printf(size of a long long int: Id byte(s)\n", sizesof(long long int)):
+
+{ 
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", Sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n" sizeof(float));
+  
+return (0);
+}
