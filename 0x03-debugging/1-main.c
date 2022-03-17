4d2199ca019d19b92a07d2679c1d5b5544664d2e
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-17 01:11:37 -0700

    My 1st task message

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -12,14 +11,15 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
+
 /*
  * while (i < 10)
  * {
+ * Infinite loop - No increase of variable
  * putchar(i);
  * }
  */
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
 
