b5376b9b72bca1608fa99d6470f6fb8e09d57f3c
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-17 13:14:07 -0700

    My 1st task message

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - ifis a digit
+ * @c: print int
+ * Return: Always 0
+ */
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+retun (1);
+}
+
+else
+{
+return (0);
+}
+}
