5bdfef05b6c55c74dda2c31db407e222f24dc745
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-18 10:07:51 -0700

    My 2nd task commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,12 @@
 #include "main.h"
-
-
 /**
  * mul - multiply
- * @a: first int
- * @b: second int
+ * @a: print int
+ * @b: print int
  * Return: product of both ints
- **/
+ */
 
 int mul(int a, int b)
 {
-return (a + b);
+return (a * b);
 }
-
