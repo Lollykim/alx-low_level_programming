1fd9e36060c0ea1025aec4765f1820988cc47ae6
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-22 12:26:19 -0700

    My zero task commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates an integer value to 98
- * @n: A pointer to an int that will be updated
+ *reset_to_98 - updates an integer value to 98
+ *@n: A pointer to an int that will be updated
  * 
  * Return: void that mans is correct
  */
 
-void reset_to_08(int *n)
+void reset_to_98(int *n)
 
 {
 *n = 98;
