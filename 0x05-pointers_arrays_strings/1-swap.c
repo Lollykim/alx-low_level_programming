0c06628070a27952c37b2ed0177d981f112054eb
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-22 12:49:09 -0700

    My 1st task message

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers of a and b
+ * @a: A pointer to an int that will be updated
+ * @b: B pointer to an int that will be updated
+ * Return: void that mmeans our answer is correct
+ */
+
+void swap_int(int *a, int *b
+{
+int  f;
+
+f = *a;
+*a = *b;
+*b = f;
+}
