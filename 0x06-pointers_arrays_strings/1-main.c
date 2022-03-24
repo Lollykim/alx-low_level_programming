6361813bcbbe9d374ffac91238827affb77a92a4
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-24 12:48:40 -0700

    My 1st task commit

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -19,7 +19,7 @@ printf("%s\n", s1);
 printf("%s", s2);
 printf("%s\n", ptr);
 ptr = _strncat(s1, s2, 1024);
-print("%s", s1);
+printf("%s", s1);
 printf("%s", s2);
 printf("%s", s2);
 printf("%s", ptr);
