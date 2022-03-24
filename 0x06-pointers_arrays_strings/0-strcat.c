cc064d5096b197239b6b3aa2c485e6d096fa1423
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-24 11:14:38 -0700

    My task 0 commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,24 @@ char *_strcat(char *dest, char *src)
 int i, j;
 
 i = 0;
-while (dest[i] != '\0')
+while (*dest != 0) 
 {
 i++;
 }
+dest++;
+
 
 j = 0;
-while (src[j] != '\0')
+while (src[j] != 0)
 {
-dest[i] = src[j];
 j++;
-i++;
 }
-dest[i] = '\0';
 
+{
+*dest = src[j];
+dest++;
+}
+*dest = '\0';
+dest -= (i + j);
 return (dest);
 }
