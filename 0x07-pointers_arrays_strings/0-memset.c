32b79c496ec6d3c225742d7fcd16a8db5f2ec3bc
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-28 12:18:20 -0700

    My task 0 commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,11 +13,11 @@ char *_memset(char *s, char b, unsigned int n)
 unsigned int i;
 
 /*Declaring FOR*/
-for (i = 0; i < n; i ++)
+for (i = 0; i < n; i++)
 {
-*(s + i) = b; /*add 1position s*/
+*(s + i) = b; /*add 1 position s*/
 
-} /*EBD FOR*/
+} /*END FOR*/
 
 return (s);
 }
