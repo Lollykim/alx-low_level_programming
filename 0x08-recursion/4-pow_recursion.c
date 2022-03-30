5cbe86c771edfc77adf4bc345864254809358539
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-30 14:21:53 -0700

    My task 4 commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,15 +6,15 @@
  * Return: pow recursion
  */
 
-int _pow_recursion(int x, int y)
+int pow_recursion(int x, int y)
 {
-if (y < 0) /*base condition*/
+if (y < 0) /*Base condition*/
 {
 return (-1);
 }
 
 else if (y != 0)
-return (x * _pow_recursion(x, y, -1)); /*Recursive call*/
+return (x * pow_recursion(x, y -1)); /*Recursive call*/
 
 else
 return (1);
