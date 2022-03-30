c5b59331dad59c01e2eb78fc06a620253c84528b
Lollykim <lollyoluwafemi545@gmail.com>
2022-03-30 12:57:12 -0700

    My task 3 commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * factoial - factorial of a gien number
+ * @n: pointers block of memory to fill
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+if (n == 0) /*Base condition*/
+
+return (1);
+
+else if (n < 0) /*Basw condition*/
+return (-1);
+else
+return (n * factorial(n - 1)); /*Recursive call*/
+
+}
