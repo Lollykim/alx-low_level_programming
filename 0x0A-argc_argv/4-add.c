83e932d172ab3cb4da30b8907f63ed586137d4bf
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-02 15:13:32 -0700

    My task 5 commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -32,10 +32,10 @@ return (1);
 
 /**
  * main - print the name of the program
- * @argc: count argument
+ * @argc: count arguments
  * @argv: Arguments
  *
- * Return: Always 0 (success)
+ * return: Always 0 (success)
  */
 
 int main(int argc, char *argv[])
