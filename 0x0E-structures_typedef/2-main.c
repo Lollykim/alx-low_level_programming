188e9b4c0fabfa46b2e0d8f5fd23ab083160a6b1
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-11 15:03:55 -0700

    My task 2 commit

---
+++ 0x0E-structures_typedef/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - chech thev code
+ *
+ * Return Always 0
+ */
+{
+struct dog my_dog;
+
+my_dog.name = "Poppy";
+my_dog.age = 3.5;
+my_dod. owunerm= "Bob";
+print_dog(&my_dog);
+return (0);
+}
