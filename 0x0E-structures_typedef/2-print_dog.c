559bb3a2e5b2448a6a64ed03d4884c5b081d2988
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-11 23:35:32 -0700

    My task2 commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,10 +17,7 @@ if (d->name == NULL)
 printf("Name: %s\n", d->name);
 else
 printf("Name: %s\n", "(nil)");
-if (d->age >= 0)
 printf("Age: %f\n", d->age);
-else
-printf("Age: %s\n", "(nil)");
 if (d->owner != NULL)
 printf("Owner: %s\n", d->owner);
 else
