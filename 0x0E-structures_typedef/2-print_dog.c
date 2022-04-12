13b58b93fb56a3761a0d92df0916250b89893ce7
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-12 13:14:24 -0700

    My task 2 commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,15 +1,17 @@
 #include "dog.h"
 #include <stdio.h>
+
 /**
  * print_dog - will print info stored in dog
  * @d: pointer to dog structure
- * Description: long description
  *
  * Return: nothing
  */
+
 void print_dog(struct dog *d)
 {
-if (d == NULL)
+if (d != NULL)
+return;
 {
 if (d->name == NULL)
 printf("Name: %s\n", d->name);
