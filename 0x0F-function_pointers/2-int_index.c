f970f060a96f7ac10cdb3493ace2e78b75fc13bd
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-13 10:54:23 -0700

    My task 2 commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: array to seach in
+ * @size: size of an array
+ * @cmp: pointer to the comparing function
+ *
+ * Return: index of the first element for which
+ * the cap function does not not return 0
+ * or -1 if no match is found or size is negative
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (!array || !cmp)
+return (-1);
+
+if (size <= 0)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (((*cmp)(array[i])) == 1)
+return (1);
+}
+return (-1);
+}
