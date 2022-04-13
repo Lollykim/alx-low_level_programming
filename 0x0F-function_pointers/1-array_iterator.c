1f369590c73de3871b05681694f44ac48570ce60
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-13 08:03:48 -0700

    My task 1  commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * array_iterator - execute function on an array
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function need to execute
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+
+if (array != NULL && action != NULL)
+{
+for (i = 0; 1 < size; i++)
+action(array[i]);
+}
+}
