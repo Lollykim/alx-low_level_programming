df11aee7513935cbcb0cf4677cb2ab49331ea1bf
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-13 14:37:22 -0700

    My task 4 commit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <stdio.h
+#include <stdio.h>
 /**
  * main - print its own upcodes
  * @argc: number of arguments
@@ -16,7 +16,7 @@ p = (char *)main;
 
 if (argc != 2)
 {
-printf("Error\n"0;
+printf("Error\n");
 exit(1);
 }
 
@@ -24,7 +24,7 @@ size = atoi(argv[1]);
 
 if (size < 0)
 {
-printf("Error\n")
+printf("Error\n");
 exit(2);
 }
 
