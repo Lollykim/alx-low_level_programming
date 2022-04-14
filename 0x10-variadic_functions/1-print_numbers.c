e801164bbfb705590cb4f431e3fda4ad21859c39
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-14 12:56:38 -0700

    add task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - print numbers followed by a new line
+ * @separator: character to be printed between numbers
+ * @n:mtotal numbers of all the parameters
+ * Return: sum of all  parameters
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+int num = 0;
+
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+{
+num = va_arg(args, int);
+if (i < (n - 1) && separator != 0)
+printf("%d%s", num, separator);
+else
+printf("%d", num);
+}
+va_end(args);
+printf('\n');
+}
