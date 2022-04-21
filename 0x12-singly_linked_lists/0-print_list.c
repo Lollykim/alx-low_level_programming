5dfbd9586b3e14602c40be9e4e7bb2892f340735
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-21 10:54:06 -0700

    MY task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "list.h"
+/**
+ * print_list - function
+ * @h: string to print
+ *
+ * Description: function yto print all elements of the list_t list
+ * Return: numbers of nodes
+ */
+size_t print_list(const list_t *h)
+{
+unsigned int i = 0;
+
+while (h != NULL)
+{
+if (h -> str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+i++
+}
+return (i);
+}
