a975c5508faca5eed3eb2ccafcf65f0e571da626
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-21 12:39:17 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - function
+ * @head: a double pointer to a linked list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+list_t *temp = head;
+
+while (head != NULL)
+{
+temp = head;
+
+free(temp->str);
+free(temp);
+head = head->next;
+}
