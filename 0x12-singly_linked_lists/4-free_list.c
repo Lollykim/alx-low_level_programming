276ea5a07b6b44ee744d4f8aef467df3c1de940e
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-21 13:00:55 -0700

    task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,17 +2,19 @@
 /**
  * free_list - function
  * @head: a double pointer to a linked list
- * Return: nothing
+ *
+ * Description: function to free list
+ * Return: 0
  */
 void free_list(list_t *head)
 {
-list_t *temp = head;
+list_t *temp = head; /* will store head ->next*/
 
 while (head != NULL)
 {
 temp = head;
-
+/* free the stuff inside what head is pointing to */
 free(temp->str);
 free(temp);
-head = head->next;
+head = head->next; /* goes to next node in life and repeat process*/
 }
