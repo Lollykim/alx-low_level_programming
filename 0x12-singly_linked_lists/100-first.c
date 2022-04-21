3517c53b22cefb01b42df32439e9139e2544da9a
Lollykim <lollyoluwafemi545@gmail.com>
2022-04-21 13:27:22 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,10 +4,12 @@
  * show_list - executes before the main function
  *
  * Return: nothing
- */\void startup(void)
+ */
+void startup(void)
 {
 char *str;
 
 str = "You're beat! and yet, you must allow, \nI bore my house upon my back!\n";
 printf("%s", str);
 }
+
