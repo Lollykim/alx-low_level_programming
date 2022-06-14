20461dfd092ef0cb9e7b0b79e2b5ea33a47a5451
Lollykim <lollyoluwafemi545@gmail.com>
2022-06-13 17:48:31 -0700

    task done

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -c -Wall -Werror -Wextra -pedantic -fpic *.c
-gcc -shared *.o -o liball.so
+gcc -c -Wall -Werror -Wextra -pedantic -std=gnu89 -fpic *.c
+gcc -shared -o liball.so *.o
