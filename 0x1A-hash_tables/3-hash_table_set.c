84654b907cd534fda9e2d76ebfd78c9f0f6e37f0
Lollykim <lollyoluwafemi545@gmail.com>
2022-07-06 16:02:42 -0700

    Task added as correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,33 +1,87 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds an element to the hash table
- * @ht: hash table to add to
- * @key: key to add
- * @value: value to store for key
+ * create_and_add_node - malloc, set values, and insert node into hash table
+ * @ht: hash table
+ * @key: key; can't be empty string
+ * @value: value
+ * @idx: index to insert in at hash table
+ * Return: 1 if success, 0 if fail
+ */
+int create_and_add_node(hash_table_t *ht, const char *key, const char *value,
+			unsigned long int idx)
+{
+	hash_node_t *node = NULL;
+	char *k;
+	char *v;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+
+	k = strdup(key);
+	if (!k)
+	{
+		free(node);
+		return (0);
+	}
+
+	v = strdup(value);
+	if (!v)
+	{
+		free(k);
+		free(node);
+		return (0);
+	}
+
+	node->key = k;
+	node->value = v;
+
+	if ((ht->array)[idx] == NULL)
+		node->next = NULL;
+	else
+		node->next = (ht->array)[idx];
+	(ht->array)[idx] = node;
+
+	return (1);
+}
+
+/**
+ * hash_table_set - add element to hash table
+ * @ht: hash table
+ * @key: key; can't be empty string
+ * @value: value
  * Return: 1 if success, 0 if fail
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newpair, *tmp;
-	unsigned long int idx;
+	/* get index */
+	/* if key already exists, update value and return */
+	/* else create node */
+	/* set ht idx ptr to node; else add node to front if collision */
 
-	if (ht == NULL || key == NULL || value == NULL)
+	unsigned long int idx;
+	hash_node_t *node = NULL;
+	char *v;
+
+	if (!ht || !(ht->array) || !key || strlen(key) == 0 || !value)
 		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[idx];
 
-	if (tmp != NULL)
+	node = (ht->array)[idx];
+	while (node && (strcmp(key, node->key) != 0))
+		node = node->next;
+	if (node != NULL)
 	{
-		while (tmp)
-		{
-			if (strcmp(tmp->key, key) == 0)
-			{
-				tmp->value = strdup(value);
-				return (1);
-			}
-			tmp = tmp->next;
-		}
+		v = strdup(value);
+		if (!v)
+			return (0);
+		if (node->value)
+			free(node->value);
+		node->value = v;
+		return (1);
 	}
+
+	return (create_and_add_node(ht, key, value, idx));
 }	
